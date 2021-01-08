8db2574844c2039982192193b29eec3ca4f7d4cc
Sudha kumari <57556648+sudha123kumari@users.noreply.github.com>
2021-01-08 11:22:27 +0530

    Resources page modified

--- docs/resource_page/script.js
+++ docs/resource_page/script.js
@@ -12,3 +12,20 @@ for (i = 0; i < acc.length; i++) {
       }
    });
 }
+/*
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
+*/
