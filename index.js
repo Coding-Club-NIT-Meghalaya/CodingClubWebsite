85b8a1fa0fac53a094433fb07e9921146a43b331
Govind Kumar <govindkumar3006@gmail.com>
2021-02-16 13:04:48 +0530

    landing page routed

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 var express = require("express");
 var app = express();
-app.set('view engine', 'html');
+const path = require('path');
+// app.set('view engine', 'html');
 
-app.engine('html', require('ejs').renderFile);
+// app.engine('html', require('ejs').renderFile);
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set("views", __dirname + "/views"); // set express to look in this folder to render our view
-app.get("/",function(req,res)
-{
-res.render("index.html")
+app.get("/", function (req, res) {
+  res.render("index");
 });
-app.get("/events",function(req,res)
-{
-res.render("Event_2.0/event.html")
+app.get("/events", function (req, res) {
+  res.render("Event_2.0/event.html");
 });
-app.get("/resources",function(req,res)
-{
-res.render("resource_page/index.html")
+app.get("/resources", function (req, res) {
+  res.render("resource_page/index.html");
 });
-app.get("/projects",function(req,res)
-{
-res.render("Project_page/index.html")
+app.get("/projects", function (req, res) {
+  res.render("Project_page/index.html");
 });
-app.get("/team",function(req,res)
-{
-res.render("team_page/index.html")
+app.get("/team", function (req, res) {
+  res.render("team_page/index.html");
 });
 
-
-const port= 8000;
+const port = 8000;
 app.set("port", process.env.port || port); // set express to use this port
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-  });
-  
+  console.log(`Server running on port: ${port}`);
+});
--- public/landing_page/assets/scripts/animate.js
+++ public/landing_page/assets/scripts/animate.js
@@ -6,7 +6,6 @@
 //     const c3=document.getElementById("cube3");
 //     const c4=document.getElementById("cube4");
 
-
 //     gsap.from(c1,{
 //         opacity:0.6,
 //         ease:Power2.easeOut,
@@ -54,26 +53,27 @@
 //  window.addEventListener("resize", init);
 //  init();
 function navSlide() {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
-    
-    burger.addEventListener("click", () => {
-        //Toggle Nav
-        nav.classList.toggle("nav-active");
-        
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ""
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle("toggle");
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
     });
-    
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
 }
 
 navSlide();
