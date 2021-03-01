d9a8d60f90128e7bad3e51705d5111d80c9c277c
Govind Kumar <govindkumar3006@gmail.com>
2021-03-01 17:23:00 +0530

    popup added

--- public/landing_page/assets/scripts/animate.js
+++ public/landing_page/assets/scripts/animate.js
@@ -77,3 +77,28 @@ function navSlide() {
 }
 
 navSlide();
+
+
+
+////////////// popup ////////////////////////////////////////
+
+$(function() {
+  
+  // contact form animations
+  $('#contact').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////
