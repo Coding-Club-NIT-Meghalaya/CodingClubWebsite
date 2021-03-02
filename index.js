907ab57ade831a2c4ebc7a0c84e96b2d94267e46
Govind Kumar <govindkumar3006@gmail.com>
2021-03-02 21:16:15 +0530

    added blog feature

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 var express = require("express");
 var app = express();
 const path = require('path');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { connected } = require("process");
+
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("views", __dirname + "/views"); // set express to look in this folder to render our view
+
+
+//MONGO setup
+mongoose.connect('mongodb+srv://Blogs:Fuu1act2J6PwKodj@codingclub.p5vjk.mongodb.net/blog?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})  
+
+//model config
+var blogSchema= new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  writtenBy: String,
+  created: {type: Date, default:Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema)
+
 app.get("/", function (req, res) {
   res.render("index");
 });
 
-
 app.get("/events", function (req, res) {
   res.render("event");
 });
+
 app.get("/resources", function (req, res) {
-  res.render("resource");
+  const f = Blog.find((err, obj)=>{
+    if(err){
+      res.send("error occured");
+    }else
+    res.render("resource", {blog: obj});
+  })
+
 });
 app.get("/projects", function (req, res) {
   res.render("project");
@@ -24,17 +53,43 @@ app.get("/teams", function (req, res) {
   res.render("team");
 });
 
-// const port = 8000;
-// app.set("port", process.env.port || port); // set express to use this port
+app.get("/blogs/new", (req, res)=>{
+  res.render("newBlog");
+})
 
-// var port_number = server.listen(process.env.PORT || 3000);
-// app.listen(port_number);
+//create
+app.post("/blogs", function(req, res){
+  // console.log(req.body.blog);
+  Blog.create(req.body.blog, (err, newBlog)=>{
+    if(err){
+      alert("Please fill the details correctly");
+      res.render("newBlog");
+    }else{
+      res.redirect("resources");
+    }
+  })
+})
 
-// app.listen(port, () => {
-//   console.log(`Server running on port: ${port}`);
-// });
+app.get("/resources/blog/:id", (req, res)=>{
+
+  let Foundid= req.params.id;
+  // console.log(Foundid);
+  Blog.findOne({_id: Foundid}, (err, foundBlog)=>{
+    if(err){
+        res.redirect("resources");
+    }else{
+      console.log(foundBlog);
+      res.render("showBlog", {blog: foundBlog});
+    }
+  })
+})
+
+const port = 8000;
+app.set("port", process.env.PORT || port); // set express to use this port
 
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("listening on http://localhost:3000/");
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
 });
+
+
