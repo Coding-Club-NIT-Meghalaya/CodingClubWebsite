f607faf0c7825067ab2cf59e7d99f097c2294980
Utkarsh Kumar <utkarshkumar965@gmail.com>
2021-03-02 22:28:31 +0530

    Connected Mongodb Atlas

--- index.js
+++ index.js
@@ -2,30 +2,42 @@ var express = require("express");
 var app = express();
 const path = require('path');
 const mongoose = require("mongoose");
+const db = mongoose.connection;
 const bodyParser = require("body-parser");
-const { connected } = require("process");
-
+const port = process.env.PORT || 8000;
+const {
+  connected
+} = require("process");
+app.use(express.urlencoded());
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set("views", __dirname + "/views"); // set express to look in this folder to render our view
 
 
 //MONGO setup
-mongoose.connect('mongodb+srv://Blogs:Fuu1act2J6PwKodj@codingclub.p5vjk.mongodb.net/blog?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://Blogs:H0a4qJJJjrakvT07@codingclub.p5vjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})  
-
+});
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("We are connected");
+});
 //model config
-var blogSchema= new mongoose.Schema({
+var blogSchema = new mongoose.Schema({
   title: String,
   image: String,
   body: String,
   writtenBy: String,
-  created: {type: Date, default:Date.now}
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 var Blog = mongoose.model("Blog", blogSchema)
 
@@ -38,11 +50,13 @@ app.get("/events", function (req, res) {
 });
 
 app.get("/resources", function (req, res) {
-  const f = Blog.find((err, obj)=>{
-    if(err){
+  const f = Blog.find((err, obj) => {
+    if (err) {
       res.send("error occured");
-    }else
-    res.render("resource", {blog: obj});
+    } else
+      res.render("resource", {
+        blog: obj
+      });
   })
 
 });
@@ -53,43 +67,42 @@ app.get("/teams", function (req, res) {
   res.render("team");
 });
 
-app.get("/blogs/new", (req, res)=>{
+app.get("/blogs/new", (req, res) => {
   res.render("newBlog");
 })
 
 //create
-app.post("/blogs", function(req, res){
+app.post("/blogs", function (req, res) {
   // console.log(req.body.blog);
-  Blog.create(req.body.blog, (err, newBlog)=>{
-    if(err){
-      alert("Please fill the details correctly");
+  Blog.create(req.body.blog, (err, newBlog) => {
+    if (err) {
+      // alert("Please fill the details correctly");
       res.render("newBlog");
-    }else{
+    } else {
       res.redirect("resources");
     }
   })
 })
 
-app.get("/resources/blog/:id", (req, res)=>{
+app.get("/resources/blog/:id", (req, res) => {
 
-  let Foundid= req.params.id;
+  let Foundid = req.params.id;
   // console.log(Foundid);
-  Blog.findOne({_id: Foundid}, (err, foundBlog)=>{
-    if(err){
-        res.redirect("resources");
-    }else{
+  Blog.findOne({
+    _id: Foundid
+  }, (err, foundBlog) => {
+    if (err) {
+      res.redirect("resources");
+    } else {
       console.log(foundBlog);
-      res.render("showBlog", {blog: foundBlog});
+      res.render("showBlog", {
+        blog: foundBlog
+      });
     }
   })
 })
-
-const port = 8000;
-app.set("port", process.env.PORT || port); // set express to use this port
 
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
-
-
