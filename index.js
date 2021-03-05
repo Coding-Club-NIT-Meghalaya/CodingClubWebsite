d3e599923cc34e8556482670946ffde990c39a3e
Utkarsh Kumar <utkarshkumar965@gmail.com>
2021-03-05 14:58:10 +0530

    Added Multer for File Upload and Added teams mongodb Schema

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 const path = require('path');
+const crypto = require('crypto');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 const mongoose = require("mongoose");
 const db = mongoose.connection;
 const bodyParser = require("body-parser");
@@ -9,7 +14,7 @@ const {
   connected
 } = require("process");
 app.use(express.urlencoded());
-
+app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
@@ -23,9 +28,34 @@ mongoose.connect('mongodb+srv://Blogs:jrim6CerRL1qlDNd@codingclub.p5vjk.mongodb.
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+let gfs;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
+db.once('open', () => {
   console.log("We are connected");
+  //Init stream
+  gfs = Grid(db.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+const storage = new GridFsStorage({
+  url: 'mongodb+srv://Blogs:jrim6CerRL1qlDNd@codingclub.p5vjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({
+  storage
 });
 //model config
 var blogSchema = new mongoose.Schema({
@@ -38,7 +68,18 @@ var blogSchema = new mongoose.Schema({
     default: Date.now
   }
 });
+var memberSchema = new mongoose.Schema({
+  FirstName: String,
+  LastName: String,
+  Designation: Number,
+  DesignationName: String,
+  Github: String,
+  LinkedIn: String,
+  Instagram: String,
+  filename: String,
+});
 var Blog = mongoose.model("Blog", blogSchema)
+var TeamMember = mongoose.model("TeamMember", memberSchema);
 
 app.get("/", function (req, res) {
   res.render("index");
@@ -46,6 +87,9 @@ app.get("/", function (req, res) {
 
 app.get("/events", function (req, res) {
   res.render("event");
+});
+app.get("/addUser", function (req, res) {
+  res.render("addUser");
 });
 
 app.get("/resources", function (req, res) {
@@ -57,18 +101,62 @@ app.get("/resources", function (req, res) {
         blog: obj
       });
   })
-
 });
 app.get("/projects", function (req, res) {
   res.render("project");
 });
+app.get("/teamMembers", function (req, res) {
+  TeamMember.find((err, obj) => {
+    if (err)
+      res.send("Err occured");
+    else
+      res.render("/teams", {
+        arr: obj
+      }).sort({
+        Designation: 1
+      });
+  })
+});
 app.get("/teams", function (req, res) {
-  res.render("team");
+  TeamMember.find(function (err, obj) {
+    if (err)
+      res.send("Error occured");
+    else
+      res.render("team", {
+        arr: obj,
+        index: 0
+      });
+  }).sort({
+    Designation: 1
+  });
 });
 
 app.get("/blogs/new", (req, res) => {
   res.render("newBlog");
 })
+//@get for image with its filename
+app.get('/profileImage/:filename', (req, res) => {
+  gfs.files.findOne({
+    filename: req.params.filename
+  }, (err, file) => {
+    //check if file 
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file Exists'
+      });
+    }
+    //check if image
+    if (file.contentType == 'image/jpeg' || file.contentType == 'img/png') {
+      //Read output to the browser
+      const readStream = gfs.createReadStream(file.filename);
+      readStream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an Image'
+      });
+    }
+  })
+});
 
 //create
 app.post("/blogs", function (req, res) {
@@ -82,7 +170,18 @@ app.post("/blogs", function (req, res) {
     }
   })
 })
-
+//@post for adding a team member
+app.post("/adduser", upload.single('profileImage'), function (req, res) {
+  let data = req.body;
+  console.log(data);
+  data["filename"] = req.file.filename;
+  TeamMember.create(data, (err, newUser) => {
+    if (err)
+      res.send("Data Not uploaded");
+    else
+      res.redirect("adduser");
+  });
+});
 app.get("/resources/blog/:id", (req, res) => {
 
   let Foundid = req.params.id;
