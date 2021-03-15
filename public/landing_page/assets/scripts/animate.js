698816a8a0a8c776f9153a32f8d0f10504416884
Govind Kumar <govindkumar3006@gmail.com>
2021-03-15 17:01:47 +0530

    refactored and nav improved

--- public/landing_page/assets/scripts/animate.js
+++ public/landing_page/assets/scripts/animate.js
@@ -52,31 +52,6 @@
 // }
 //  window.addEventListener("resize", init);
 //  init();
-function navSlide() {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-
-  burger.addEventListener("click", () => {
-    //Toggle Nav
-    nav.classList.toggle("nav-active");
-
-    //Animate Links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.5
-        }s`;
-      }
-    });
-    //Burger Animation
-    burger.classList.toggle("toggle");
-  });
-}
-
-navSlide();
 
 
 
