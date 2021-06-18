26dbc3fb52a5aad5072a2bae5b080d9f88a936c6
Utkarsh Kumar <utkarshkanswal@gmail.com>
2021-06-18 13:43:12 +0530

    Remove cache of git modules

--- node_modules/.admin-bro-5XV1ASmw/types/src/frontend/components/app/sidebar/sidebar.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-declare type Props = {
-    isVisible: boolean;
-};
-declare const Sidebar: React.ComponentType<Props & {
-    OriginalComponent?: React.FunctionComponent<Props> | React.ComponentClass<Props, any> | undefined;
-}>;
-export { Sidebar };
-export default Sidebar;
