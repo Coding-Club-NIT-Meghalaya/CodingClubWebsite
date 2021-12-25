f65a73293419ccb22b0f72c54db813d0ebf3e6d8
Utkarsh Kumar <utkarshkanswal@gmail.com>
2021-12-25 13:21:43 +0530

    Updated Achievement

--- api_v1/achievement.js
+++ api_v1/achievement.js
@@ -101,4 +101,77 @@ router.delete('/achievement/:id', checkAuth, function (req, res, next) {
         }
     });
 });
+router.get("/update/achievement/:id", (req, res, next) => {
+    let Foundid = req.params.id;
+    Achievement.findOne({
+        _id: Foundid
+    }, (err, obj) => {
+        if (err) {
+            res.status(500).json({
+                err: err.message,
+            });
+        } else {
+            // let Date = new Date(obj.Date);
+            var date = new Date(obj.Date);
+            console.log(date);
+            var curr_date = date.getDate();
+
+            var curr_month = date.getMonth() + 1;
+
+            var curr_year = date.getFullYear();
+
+
+            date = curr_year + "-" + curr_month + "-" + curr_date;
+            console.log(date);
+            res.render("Admin/Events Page/updateAchievement", {
+                obj: obj,
+                date: date,
+            });
+        }
+    });
+});
+router.post('/update/achievement/:id', upload.single('achievementImage'), function (req, res) {
+    let newData = req.body;
+    console.log(req.body);
+    if (req.file != undefined) {
+        Achievement.findOne({
+            _id: req.params.id
+        }, (err, obj) => {
+            if (err) {
+                res.status(500).json({
+                    error: err.message
+                });
+            } else {
+
+                const options = {
+                    hostname: 'codingclubnitm.herokuapp.com',
+                    path: '/api/v1/image/del/' + req.file.filename,
+                    method: 'DELETE'
+                }
+                const hreq = https.request(options, hres => {
+                    console.log(`statusCode: ${hres.statusCode}`)
+                    console.log("sucessfull")
+                })
+                hreq.on('error', error => {
+                    console.error(error)
+                })
+                hreq.end()
+            }
+        })
+        newData["FileName"] = req.file.filename;
+    }
+    Achievement.updateOne({
+        _id: req.params.id
+    }, newData, (err, obj) => {
+        if (err) {
+            res.status(500).json({
+                error: err.message,
+            });
+        } else {
+            res.status(201).json({
+                message: 'Successfully Updated Achievement',
+            });
+        }
+    })
+});
 module.exports = router
